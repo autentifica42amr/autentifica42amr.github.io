49d794a36cbc88d770815820b70f080b448d73dc
autentifica42amr <81279447+autentifica42amr@users.noreply.github.com>
2021-03-23 23:55:42 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyDcGC5MvBJjEgtD81wfGE4vscJ9AoM0Wbg",
+    authDomain: "autentica42amr.firebaseapp.com",
+    projectId: "autentica42amr",
+    storageBucket: "autentica42amr.appspot.com",
+    messagingSenderId: "951312047054",
+    appId: "1:951312047054:web:6024cdaf7452662b34e99f",
+    measurementId: "G-NDRXFMSKQ5"
+    });
